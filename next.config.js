0af9bfdb3f56bbb5ae0578dc001bd3cb6e0634c1
tristan <romanov360@gmail.com>
2025-10-25 19:42:44 -0500

    Made it be just /jobs.

--- next.config.js
+++ next.config.js
@@ -13,6 +13,23 @@ const nextConfig = {
       },
     },
   },
+  async redirects() {
+    return [
+      {
+        source: '/dashboard/jobs',
+        destination: '/jobs',
+        permanent: true, // Use a 308 permanent redirect
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/jobs',
+        destination: '/dashboard/jobs',
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
